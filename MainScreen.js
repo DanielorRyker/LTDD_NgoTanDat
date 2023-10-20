bfa239dab4ef56db71c8bcc3a74e477b4cec5d79
BlackRyker <hoangraper2017@gmail.com>
2023-10-20 11:04:41 +0700

    create project

---
+++ MainScreen.js
@@ -0,0 +1,4 @@
+import { StyleSheet, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
